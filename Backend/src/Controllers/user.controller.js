a3455083967e72c307c2e2e73c7436a3a628705d
Lord-Rahul <rahul717321+github@gmail.com>
2025-08-23 18:21:57 +0530

    feat: add change password and update user details functionality

--- Backend/src/Controllers/user.controller.js
+++ Backend/src/Controllers/user.controller.js
@@ -2,10 +2,11 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { User } from "../models/user.model.js";
-import { options } from "../constants.js";
+import { api, options } from "../constants.js";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { response } from "express";
 
 const generateAccessAndRefreshToken = async (userId) => {
   try {
@@ -108,10 +109,101 @@ const logoutUser = asyncHandler(async (req, res) => {
     { new: true }
   );
   return res
-    .status(200,"")
+    .status(200, "")
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "user logged out successfully"));
 });
 
-export { generateAccessAndRefreshToken, registerUser, loginUser,logoutUser };
+const changeUserPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  if (!oldPassword || !newPassword) {
+    throw new ApiError(400, "password is required ");
+  }
+
+  if (newPassword.length < 6) {
+    throw new ApiError(400, "password must be 6 character long");
+  }
+
+  const user = await User.findById(req.user?._id);
+
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "wrong old password ");
+  }
+
+  const isSamePassword = await user.isPasswordCorrect(newPassword);
+
+  if (isSamePassword) {
+    throw new ApiError(400, "new password cannot be same as old password ");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res.status(200).json(new ApiResponse(200, {}, "password is changed "));
+});
+
+const update = asyncHandler(async (req, res) => {
+
+    const body = req.body || {};
+  const { fullName, email, number, address1, address2, city, pincode, state } =body;
+
+  if (
+    !fullName &&
+    !email &&
+    !number &&
+    !address1 &&
+    !address2 &&
+    !city &&
+    !pincode &&
+    !state
+  ) {
+    throw new ApiError(400, "atleast one field is required");
+  }
+
+  const updateFields = {};
+  if (fullName) updateFields.fullName = fullName;
+  if (email) updateFields.email = email.toLowerCase();
+  if (number) updateFields.number = number;
+  if (address1) updateFields.address1 = address1;
+  if (address2) updateFields.address2 = address2;
+  if (city) updateFields.city = city;
+  if (pincode) updateFields.pincode = pincode;
+  if (state) updateFields.state = state;
+
+  if (email || number) {
+    const existingUser = await User.findOne({
+      $and: [{ _id: { $ne: req.user._id } }, { $or: [{ email }, { number }] }],
+    });
+
+    if (existingUser) {
+      throw new ApiError(409, "Email or Phone number already exists");
+    }
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: updateFields },
+    { new: true }
+  ).select("-password -refreshToken");
+
+  if (!user) {
+    throw new ApiError(404, "user not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, "user details updated successfully "));
+});
+
+export {
+  generateAccessAndRefreshToken,
+  registerUser,
+  loginUser,
+  logoutUser,
+  changeUserPassword,
+  update,
+};
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,12 +1,22 @@
 import { Router } from "express";
-import { registerUser,loginUser, logoutUser } from "../Controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  changeUserPassword,
+  update,
+} from "../Controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 //register user
 router.route("/register").post(registerUser);
-router.route('/login').post(loginUser)
-router.route('/logout').post(verifyJWT,logoutUser)
+router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/changepassword").post(verifyJWT, changeUserPassword);
+router.route("/update").patch(verifyJWT,update)
+
+
 
 export default router;
