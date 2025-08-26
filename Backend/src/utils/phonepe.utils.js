c6b34eabc507620d68d3c5db86f361e60a18f2ea
Lord-Rahul <rahul717321+github@gmail.com>
2025-08-26 10:11:51 +0530

    feat: add PhonePe payment integration with configuration and utility functions
    - Created phonepe.config.js to manage PhonePe API configuration using environment variables.
    - Implemented phonepe.utils.js to handle payment creation and verification processes.
    - Added functions to generate verification hashes and interact with PhonePe's payment API.

---
+++ Backend/src/utils/phonepe.utils.js
@@ -0,0 +1,61 @@
+import crypto from "crypto";
+import axios from "axios";
+import { PHONEPE_CONFIG } from "../config/phonepe.config.js";
+import { ApiError } from "./apiError.js";
+
+export const generateXVerify = (payload, endpoint) => {
+  const string = payload + endpoint + PHONEPE_CONFIG.SALT_KEY;
+  const sha256 = crypto.createHash("sha256").update(string).digest("hex");
+  return sha256 + "###" + PHONEPE_CONFIG.SALT_INDEX;
+};
+
+export const createPhonePePayment = async (orderData) => {
+  const payload = {
+    merchantId: PHONEPE_CONFIG.MERCHANT_ID,
+    merchantTransactionId: orderData.transactionId,
+    merchantUserId: orderData.userId,
+    amount: orderData.amount * 100,
+    redirectUrl: PHONEPE_CONFIG.REDIRECT_URL,
+    redirectMode: "REDIRECT",
+    callbackUrl: PHONEPE_CONFIG.CALLBACK_URL,
+    mobileNumber: orderData.mobileNumber,
+    paymentInstrument: {
+      type: "PAY_PAGE",
+    },
+  };
+
+  const base64Payload = Buffer.from(JSON.stringify(payload)).toString("base64");
+  const xVerify = generateXVerify(base64Payload, "/pg/v1/pay");
+
+  try {
+    const response = await axios.post(
+      `${PHONEPE_CONFIG.BASE_URL}/pg/v1/pay`,
+      { request: base64Payload },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-VERIFY": xVerify,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new ApiError(`payment initiation failed ${error.message}`);
+  }
+};
+
+export const verifyPhonePePayment = async (transactionId) => {
+  const endpoint = `/pg/v1/status/${PHONEPE_CONFIG.MERCHANT_ID}/${transactionId}`;
+  const xVerify = generateXVerify("", endpoint);
+  try {
+    const response = await axios.get(`${PHONEPE_CONFIG.BASE_URL}${endpoint}`, {
+      headers: {
+        "Content-Type": "application/json",
+        "X-VERIFY": xVerify,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(`PhonePe payment verification failed: ${error.message}`);
+  }
+};
