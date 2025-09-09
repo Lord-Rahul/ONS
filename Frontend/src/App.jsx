bdfc93e72faec6a980c82537474dc2381f318853
Lord-Rahul <rahul717321+github@gmail.com>
2025-09-09 14:32:38 +0530

    Add initial CSS file for frontend styling

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
-      
-    </div>
+    <>
+    </>
   )
 }
 
